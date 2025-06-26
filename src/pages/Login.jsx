694c5d6f238285f7acb0aca6751a76378c199928
YourGitHubUsername <ramnath20102005>
2025-06-26 10:13:48 +0530

    third and last

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import axios from '../utils/axios';
 import './Login.css';
 import logo from '../assets/logo.png';
 
